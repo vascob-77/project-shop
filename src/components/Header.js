9050be5460f068e7231974d937984ee031cd935b
Vasco <vascbarreiros@gmail.com>
2022-04-08 13:24:01 +0200

    styles header ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {FiShoppingCart} from "react-icons/fi";
 import {BiSearchAlt} from "react-icons/bi";
+import styled from "styled-components";
 
 //Image
 import logoShop from "../asset/images/logo.png";
@@ -9,33 +10,121 @@ import logoShop from "../asset/images/logo.png";
 
 const Header = () => {
     return (
-            <div className="header">
-                <div className="header_logo">
-                    <img src={logoShop} />
+            <SectionHeader>
+                <div className="header-logo">
+                    <img src={logoShop} width="120"/>
                 </div>
 
-                <div className="header_search">
+                <div className="header-search">
                     <input type="text" className="input-search"></input>
-                    <BiSearchAlt/>
+                    <BiSearchAlt className="header-logoSearch"/>
                 </div>
 
-                <div className="header_nav">
+                <div className="header-nav">
                     <div className="nav-item">
-                        <span className="item-name">Hello Guest</span>
-                        <span className="item-sign">Sign In</span>
+                        <span className="item-one">Hello Guest</span>
+                        <span className="item-two">Sign In</span>
                     </div>
                     <div className="nav-item">
-                        <span className="item-your">Your</span>
-                        <span className="item-shop">Shop</span>
+                        <span className="item-one">Your</span>
+                        <span className="item-two">Shop</span>
                     </div>
-                    <div className="nav-item">
+                    <div className="item-basket">
                         <FiShoppingCart size={35}/>
-                        <span className="item-cart">0</span>
+                        <span className="item-basket basketCount">0</span>
                     </div>
 
                 </div>
-            </div>
+            </SectionHeader>
     );
 };
+
+const SectionHeader = styled.section`
+  height: 130px;
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top:0;
+  z-index: 100;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  .header-logo{
+    display: flex;
+    align-items: center;
+    margin: 0 25px;
+  }
+  
+  .header-search{
+    display: flex;
+    flex:1;
+    align-items: center;
+    border-radius:24px;
+    margin:0 10px;
+  }
+  
+  .input-search{
+    height: 12px;
+    width: 100%;
+    padding:8px;
+    border-radius: 1px;
+  }
+  
+  .header-nav{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+  }
+  
+  .nav-item{
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+    margin-right: 10px;
+    font-weight: 700;
+  }
+  
+  .item-basket{
+    display: flex;
+    align-items: center;
+    color:white;
+    margin-right: 50px;
+    margin-left: 10px;
+  }
+  
+  .header-logoSearch{
+    padding:5px;
+    height: 22px !important;
+    background-color: #FFCE2E;
+    border-radius: 4px;
+  }
+  
+  .header-nav{
+    display: flex;
+    justify-content: space-evenly;
+  }
+  
+  .item-one{
+    font-size: 12px;
+  }
+  
+  .item-two{
+    font-size: 14px;
+    font-weight: 800;
+  }
+  
+  .item-basket{
+    display: flex;
+    color:black;
+    align-items: center;
+    justify-content: center;
+    margin-right: 50px;
+    margin-left: 10px;
+  }
+  
+  .basket-count{
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+`
 
 export default Header;
